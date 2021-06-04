6f810bc598b6c39454647ef471368d506559e583
DevJolo <bartjoseph19@gmail.com>
2021-06-04 14:55:03 +0800

    Edit feature updated

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,32 +1,65 @@
-// import { useState } from "react";
-import { MdDeleteForever } from "react-icons/md";
-// import { MdEdit } from "react-icons/md";
+import { useState, useRef } from "react";
+import { MdDeleteForever, MdEdit, MdCheck } from "react-icons/md";
 
 const Note = ({ id, text, date, handleDeleteNote, handleEditNote }) => {
+  const [editMode, setEditMode] = useState(false);
+  const inputRef = useRef(true);
+
+  const changeFocus = () => {
+    setEditMode(true);
+    inputRef.current.disabled = false;
+    inputRef.current.focus();
+  };
+
+  const update = (e) => {
+    inputRef.current.disabled = true;
+    setEditMode(false);
+  };
+
+  const moveCaretAtEnd = (e) => {
+    var temp_value = e.target.value;
+    e.target.value = "";
+    e.target.value = temp_value;
+  };
+
   return (
     <div className="note">
-      <input
+      <textarea
         className="text-value"
-        value={text}
+        ref={inputRef}
+        disabled={inputRef}
+        defaultValue={text}
+        onFocus={moveCaretAtEnd}
         onChange={(e) => {
           handleEditNote(e.target.value, id);
         }}
-      ></input>
+      />
       <div className="note-footer">
         <small>{date}</small>
+
         <div>
-          {/* <MdEdit
-            className="delete-icon"
-            size="1.3em"
-            onClick={handleIsEditClick}
-          /> */}
-          <MdDeleteForever
-            className="delete-icon"
-            onClick={() => {
-              handleDeleteNote(id);
-            }}
-            size="1.3em"
-          />
+          {editMode ? (
+            <MdCheck
+              className="delete-icon"
+              size="1.3em"
+              onClick={(e) => update(inputRef.current.value, e)}
+            />
+          ) : (
+            <>
+              <MdEdit
+                className="delete-icon"
+                size="1.3em"
+                onClick={() => changeFocus()}
+              />
+              <MdDeleteForever
+                className="delete-icon"
+                onClick={() => {
+                  handleDeleteNote(id);
+                }}
+                size="1.3em"
+              />
+            </>
+          )}
         </div>
       </div>
     </div>
